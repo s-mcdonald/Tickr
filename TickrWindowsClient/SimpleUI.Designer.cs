2be60576d0271a8c5d4078bed3277c69f576b31f
SamM <s.mcdonald@outlook.com.au>
2025-05-17 14:24:01 +1000

    updated Win client to check for state

--- TickrWindowsClient/SimpleUI.Designer.cs
+++ TickrWindowsClient/SimpleUI.Designer.cs
@@ -28,24 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.toolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageDebug = new System.Windows.Forms.TabPage();
+            this.tabPageTest = new System.Windows.Forms.TabPage();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.toolStripContainer.ContentPanel.SuspendLayout();
+            this.toolStripContainer.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabPageDebug.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(26, 25);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(122, 55);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtTest
+            // 
+            this.txtTest.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTest.Location = new System.Drawing.Point(4, 4);
+            this.txtTest.Multiline = true;
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(1438, 1060);
+            this.txtTest.TabIndex = 1;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.btnTest);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.tabControl);
+            this.splitContainer.Size = new System.Drawing.Size(1018, 706);
+            this.splitContainer.SplitterDistance = 83;
+            this.splitContainer.TabIndex = 2;
+            // 
+            // toolStripContainer
+            // 
+            // 
+            // toolStripContainer.ContentPanel
+            // 
+            this.toolStripContainer.ContentPanel.Controls.Add(this.splitContainer);
+            this.toolStripContainer.ContentPanel.Size = new System.Drawing.Size(1018, 706);
+            this.toolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer.Name = "toolStripContainer";
+            this.toolStripContainer.Size = new System.Drawing.Size(1018, 731);
+            this.toolStripContainer.TabIndex = 3;
+            this.toolStripContainer.Text = "toolStripContainer1";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPageDebug);
+            this.tabControl.Controls.Add(this.tabPageTest);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1164, 883);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPageDebug
+            // 
+            this.tabPageDebug.Controls.Add(this.txtTest);
+            this.tabPageDebug.Location = new System.Drawing.Point(4, 25);
+            this.tabPageDebug.Name = "tabPageDebug";
+            this.tabPageDebug.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDebug.Size = new System.Drawing.Size(1156, 854);
+            this.tabPageDebug.TabIndex = 0;
+            this.tabPageDebug.Text = "Debug";
+            this.tabPageDebug.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTest
+            // 
+            this.tabPageTest.Location = new System.Drawing.Point(4, 25);
+            this.tabPageTest.Name = "tabPageTest";
+            this.tabPageTest.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageTest.Size = new System.Drawing.Size(1156, 854);
+            this.tabPageTest.TabIndex = 1;
+            this.tabPageTest.Text = "Test";
+            this.tabPageTest.UseVisualStyleBackColor = true;
             // 
             // SimpleUI
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1006, 575);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.ClientSize = new System.Drawing.Size(1018, 731);
+            this.Controls.Add(this.toolStripContainer);
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "SimpleUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tickr (Windows Client)";
             this.TopMost = true;
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.ResumeLayout(false);
+            this.toolStripContainer.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer.ResumeLayout(false);
+            this.toolStripContainer.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabPageDebug.ResumeLayout(false);
+            this.tabPageDebug.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageDebug;
+        private System.Windows.Forms.TabPage tabPageTest;
     }
 }
 
--- TickrWindowsClient/SimpleUI.cs
+++ TickrWindowsClient/SimpleUI.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +16,34 @@ namespace TickrWindowsClient
         public SimpleUI()
         {
             InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls;
+
+                string apiUrl = "http://localhost:5112/tickr/status/all"; 
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string jsonResponse = reader.ReadToEnd();
+                    txtTest.Text = jsonResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                txtTest.Text = ex.Message;
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
     }
 }
