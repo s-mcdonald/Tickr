ecd3e0afc4768b5c04288e7d17c13ebcd5f8b718
SamM <s.mcdonald@outlook.com.au>
2025-05-17 13:25:53 +1000

    added initial forms project files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## Tickr
+
+> **Note:** This is a personal project for educational purposes and is currently in active development.
+
+Tickr is a C#-based web service designed to help investors analyze stock market ticker symbols and efficiently manage their investment portfolios. Whether you're a beginner or an experienced trader, Tickr offers a user-friendly interface and powerful features to enhance your investment strategy.
+
+
+🚀 Features
+Symbol Discovery: Browse and search for all available stock ticker symbols.
+My Symbols: Maintain a personalized list of your favorite or watched stocks.
+Buy/Sell Triggers: Set custom alerts for buying or selling stocks based on price thresholds.
+Real-Time Charts: Visualize stock performance with interactive charts.
+Portfolio Management: Track your investments and monitor portfolio performance.
+
+
+📦 Tech Stack
+Backend: C# with ASP.NET Core
+Database: SQL Server
+Frontend: 
+    Web: HTML, CSS, JavaScript (Optional for future enhancements)
+    WinForms: A Native WinForms client
---
+++ TickrWindowsClient/SimpleUI.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace TickrWindowsClient
+{
+    partial class SimpleUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SimpleUI
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1006, 575);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "SimpleUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tickr (Windows Client)";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
